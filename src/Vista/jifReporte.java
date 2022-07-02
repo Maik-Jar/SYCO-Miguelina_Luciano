1b958e983d16c2cfcbd8b5de8bd71417480415f1
Brian Rivera <maikrivera01@gmail.com>
2022-07-02 17:39:25 -0400

    modificacion pantalla reportes

--- src/Vista/jifReporte.java
+++ src/Vista/jifReporte.java
@@ -22,7 +22,7 @@ public class jifReporte extends javax.swing.JInternalFrame {
     Usuario usr;
     
     private void Configuracionventana() {
-        this.setSize(414,302);
+        this.setSize(390,500);
         //this.setSize(460,302);
     }
     
@@ -30,6 +30,7 @@ public class jifReporte extends javax.swing.JInternalFrame {
         initComponents();
         Configuracionventana();
         this.usr = usr;
+  
         if ("Usuario".equals(usr.getRol())) {
             cbUsuario.setVisible(false);
             lbUsuario.setVisible(false);
@@ -39,6 +40,7 @@ public class jifReporte extends javax.swing.JInternalFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
         jPanel1 = new javax.swing.JPanel();
         lbFechadesde = new javax.swing.JLabel();
@@ -57,119 +59,76 @@ public class jifReporte extends javax.swing.JInternalFrame {
         setTitle("Reporte de Facturación");
         setFrameIcon(new javax.swing.ImageIcon(getClass().getResource("/RecursosGraficos/reporte.png"))); // NOI18N
 
-        lbFechadesde.setFont(new java.awt.Font("Tw Cen MT Condensed", 1, 18)); // NOI18N
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lbFechadesde.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
         lbFechadesde.setForeground(new java.awt.Color(118, 113, 113));
+        lbFechadesde.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         lbFechadesde.setText("Desde*");
+        jPanel1.add(lbFechadesde, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 100, 40));
 
-        lbEstatus.setFont(new java.awt.Font("Tw Cen MT Condensed", 1, 18)); // NOI18N
+        lbEstatus.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
         lbEstatus.setForeground(new java.awt.Color(118, 113, 113));
+        lbEstatus.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         lbEstatus.setText("Estatus");
+        jPanel1.add(lbEstatus, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 140, 100, 40));
 
-        cbEstatus.setFont(new java.awt.Font("Browallia New", 0, 22)); // NOI18N
+        cbEstatus.setFont(new java.awt.Font("Roboto Light", 0, 22)); // NOI18N
         cbEstatus.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Todos", "ACTIVO", "INACTIVO" }));
+        jPanel1.add(cbEstatus, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 140, 200, 40));
 
-        dcFechadesde.setDateFormatString("yyyy-MM-dd");
-        dcFechadesde.setFont(new java.awt.Font("Browallia New", 0, 22)); // NOI18N
+        dcFechadesde.setDateFormatString("dd-MM-yyyy");
+        dcFechadesde.setFont(new java.awt.Font("Roboto Light", 0, 22)); // NOI18N
+        jPanel1.add(dcFechadesde, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 20, 200, 40));
 
-        btCerrar.setFont(new java.awt.Font("Tw Cen MT Condensed", 1, 18)); // NOI18N
+        btCerrar.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
         btCerrar.setText("Cerrar");
         btCerrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btCerrarActionPerformed(evt);
             }
         });
+        jPanel1.add(btCerrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 410, 150, 40));
 
-        btGenerar.setFont(new java.awt.Font("Tw Cen MT Condensed", 1, 18)); // NOI18N
+        btGenerar.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
         btGenerar.setText("Generar");
         btGenerar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btGenerarActionPerformed(evt);
             }
         });
+        jPanel1.add(btGenerar, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 410, 150, 40));
 
-        lbFechahasta.setFont(new java.awt.Font("Tw Cen MT Condensed", 1, 18)); // NOI18N
+        lbFechahasta.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
         lbFechahasta.setForeground(new java.awt.Color(118, 113, 113));
+        lbFechahasta.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         lbFechahasta.setText("Hasta*");
+        jPanel1.add(lbFechahasta, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 80, 100, 40));
 
-        dcFechahasta.setDateFormatString("yyyy-MM-dd");
-        dcFechahasta.setFont(new java.awt.Font("Browallia New", 0, 22)); // NOI18N
+        dcFechahasta.setDateFormatString("dd-MM-yyyy");
+        dcFechahasta.setFont(new java.awt.Font("Roboto Light", 0, 22)); // NOI18N
+        jPanel1.add(dcFechahasta, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 80, 200, 40));
 
-        lbUsuario.setFont(new java.awt.Font("Tw Cen MT Condensed", 1, 18)); // NOI18N
+        lbUsuario.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
         lbUsuario.setForeground(new java.awt.Color(118, 113, 113));
+        lbUsuario.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         lbUsuario.setText("Usuario");
+        jPanel1.add(lbUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 200, 100, 40));
 
-        cbUsuario.setFont(new java.awt.Font("Browallia New", 0, 22)); // NOI18N
+        cbUsuario.setFont(new java.awt.Font("Roboto Light", 0, 22)); // NOI18N
         cbUsuario.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Todos", "Por usuario" }));
         cbUsuario.setSelectedIndex(1);
+        jPanel1.add(cbUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 200, 200, 40));
 
-        lbReporte.setFont(new java.awt.Font("Tw Cen MT Condensed", 1, 18)); // NOI18N
+        lbReporte.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
         lbReporte.setForeground(new java.awt.Color(118, 113, 113));
+        lbReporte.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         lbReporte.setText("Reporte");
+        jPanel1.add(lbReporte, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 260, 100, 40));
 
-        cbReporte.setFont(new java.awt.Font("Browallia New", 0, 22)); // NOI18N
+        cbReporte.setFont(new java.awt.Font("Roboto Light", 0, 22)); // NOI18N
         cbReporte.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Simple", "Detallado" }));
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(lbReporte)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(cbReporte, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(btGenerar)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(btCerrar))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(lbUsuario)
-                                .addComponent(lbEstatus, javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(lbFechadesde, javax.swing.GroupLayout.Alignment.TRAILING))
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(jPanel1Layout.createSequentialGroup()
-                                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(dcFechadesde, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(cbEstatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addGap(33, 33, 33)
-                                    .addComponent(lbFechahasta)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(dcFechahasta, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addComponent(cbUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addContainerGap(22, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(24, 24, 24)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(dcFechadesde, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(dcFechahasta, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lbFechahasta)
-                    .addComponent(lbFechadesde))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbEstatus)
-                    .addComponent(cbEstatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbUsuario)
-                    .addComponent(cbUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbReporte)
-                    .addComponent(cbReporte, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btCerrar)
-                    .addComponent(btGenerar))
-                .addContainerGap())
-        );
+        jPanel1.add(cbReporte, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 260, 200, 40));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -179,7 +138,7 @@ public class jifReporte extends javax.swing.JInternalFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 591, Short.MAX_VALUE)
         );
 
         pack();
@@ -195,48 +154,52 @@ public class jifReporte extends javax.swing.JInternalFrame {
         ArrayList coleccion = new ArrayList();
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         
-        try {
+        if(dcFechadesde.getDate() != null && dcFechahasta.getDate() != null){
+        
+            try {
 
-            // Conexión a la base de datos.
-            Connection conn = con.getConnection();
+                // Conexión a la base de datos.
+                Connection conn = con.getConnection();
 
-            // Consulta a la base de datos.
-            ps = conn.prepareCall("{ call jifReporte_BUSCAR_REPORTE (?,?,?,?) }");
+                // Consulta a la base de datos.
+                ps = conn.prepareCall("{ call jifReporte_BUSCAR_REPORTE (?,?,?,?) }");
 
-            // Parametros de consulta.
-            ps.setString(1, dateFormat.format(dcFechadesde.getDate())); // Envia la fecha desde.
-            ps.setString(2, dateFormat.format(dcFechahasta.getDate())); // Envia la fecha hasta.
-            ps.setString(3, cbEstatus.getSelectedItem().toString()); // Envia el estatus.
-            if (cbUsuario.getSelectedIndex() == 1) {
-                ps.setString(4, usr.getNick()); // Envia el usuario.
-            } else {
-                ps.setString(4, cbUsuario.getSelectedItem().toString()); // Envia el usuario.
-            }
-
-            rs = ps.executeQuery();
-
-            if (cbUsuario.getSelectedIndex() == 1) {
-                while (rs.next()) {
-                    red = new RepFacturacion(usr.getNick(), dateFormat.format(dcFechadesde.getDate()), dateFormat.format(dcFechahasta.getDate()), rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7));
-                    coleccion.add(red);
+                // Parametros de consulta.
+                ps.setString(1, dateFormat.format(dcFechadesde.getDate())); // Envia la fecha desde.
+                ps.setString(2, dateFormat.format(dcFechahasta.getDate())); // Envia la fecha hasta.
+                ps.setString(3, cbEstatus.getSelectedItem().toString()); // Envia el estatus.
+                if (cbUsuario.getSelectedIndex() == 1) {
+                    ps.setString(4, usr.getNick()); // Envia el usuario.
+                } else {
+                    ps.setString(4, cbUsuario.getSelectedItem().toString()); // Envia el usuario.
                 }
-            } else {
-                while (rs.next()) {
-                    red = new RepFacturacion(cbUsuario.getSelectedItem().toString(), dateFormat.format(dcFechadesde.getDate()), dateFormat.format(dcFechahasta.getDate()), rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7));
-                    coleccion.add(red);
-                }
-            }
 
-            //Cierre de conexión a la base de datos.
-            conn.close();
-            
-            reporte.Rep_facturacion(coleccion);
-            
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage(), "BASE DE DATOS", JOptionPane.ERROR_MESSAGE);
-            System.out.println(e);
+                rs = ps.executeQuery();
+
+                if (cbUsuario.getSelectedIndex() == 1) {
+                    while (rs.next()) {
+                        red = new RepFacturacion(usr.getNick(), dateFormat.format(dcFechadesde.getDate()), dateFormat.format(dcFechahasta.getDate()), rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7));
+                        coleccion.add(red);
+                    }
+                } else {
+                    while (rs.next()) {
+                        red = new RepFacturacion(cbUsuario.getSelectedItem().toString(), dateFormat.format(dcFechadesde.getDate()), dateFormat.format(dcFechahasta.getDate()), rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7));
+                        coleccion.add(red);
+                    }
+                }
+
+                //Cierre de conexión a la base de datos.
+                conn.close();
+
+                reporte.Rep_facturacion(coleccion);
+
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "BASE DE DATOS", JOptionPane.ERROR_MESSAGE);
+                System.out.println(e);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Por favor complete el rango de fechas.", "Rango de fechas incompleto", JOptionPane.ERROR_MESSAGE);
         }
-       
     }//GEN-LAST:event_btGenerarActionPerformed
 
 
